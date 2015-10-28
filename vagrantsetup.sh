1680ce3abf917f83a34599c514c709ef981389a3
Didier Roche <didrocks@ubuntu.com>
2015-10-28 17:55:51 +0100

    Fix comment and better wording

--- vagrantsetup.sh
+++ vagrantsetup.sh
@@ -6,14 +6,15 @@ set -e
 zypper --non-interactive addrepo -Gf http://download.opensuse.org/repositories/Virtualization:containers/openSUSE_13.2/Virtualization:containers.repo
 zypper --non-interactive install kubernetes-master kubernetes-node etcd docker
 
-# there is a systemd unit dependency order issue between etcd and kubernetes api server in this repo
+# there is a systemd unit dependency ordering issue between etcd and kubernetes api server in this repo
 # etcd is After=multi-user.target but WantedBy=multi-user.target
 # kube-apiserver is: After=etcd.service but WantedBy=multi-user.target
-# systemd choose to not start kube-apiserver to resolve this loop. We remote the After=multi-user.target in etcd
-# (which doesn't makes sense for a wanted by)
+# systemd chooses (visible in the journal) to not start kube-apiserver to resolve this loop. We remove the
+# After=multi-user.target in etcd (which doesn't makes sense for a wanted by) and override the unit by
+# putting a new one in /etc
 sed 's/After=multi-user.target//' /usr/lib/systemd/system/etcd.service > /etc/systemd/system/etcd.service
 
-# create ServiceAccount adminission controller keys missing when setting up kubernetes packages
+# create ServiceAccount admission controller keys missing when setting up kubernetes packages
 # https://github.com/kubernetes/kubernetes/issues/11355#issuecomment-127378691
 ssl_keyfile=/etc/kubernetes/serviceaccount.key
 openssl genrsa -out $ssl_keyfile 2048
